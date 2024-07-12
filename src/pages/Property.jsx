4bb379acf67b5d734fdc42eabded9c26a19a9ee7
Kevin Lavier <kevinlavier@orange.fr>
2024-07-12 13:55:29 +0200

    feat: add error page when user navigate to the wrong property id

--- src/pages/Property.jsx
+++ src/pages/Property.jsx
@@ -4,30 +4,36 @@ import logements from '../datas/logements.json';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Carrousel from "../components/Carrousel";
-import Details from "../components/Details"
-
+import Details from "../components/Details";
+import NotFound from './NotFound';
 
 const Property = () => {
   const { id } = useParams();
   const [rental, setRental] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const foundRental = logements.find((logement) => logement.id === id);
     setRental(foundRental);
+    setLoading(false);
   }, [id]);
 
-  if (!rental) {
+  if (loading) {
     return <div>Loading...</div>;
+  }
+
+  if (!rental) {
+    return <NotFound />;
   }
 
   return (
     <div>
       <Header />
       <div className="carrousel-wrapper">
-      <Carrousel images={rental.pictures} />
+        <Carrousel images={rental.pictures} />
       </div>
       <div className="details-wrapper">
-      <Details rental={rental} />
+        <Details rental={rental} />
       </div>
       <Footer />
     </div>
