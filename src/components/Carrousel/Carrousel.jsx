edfe5f8a3e1fa324c5aa867becfe56d915c54628
Kevin Lavier <kevinlavier@orange.fr>
2024-07-27 14:09:21 +0200

    fix : index carrousel in responsive display:none / add condition images.length > 1 : hide arrows

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -12,11 +12,15 @@ const Carrousel = ({ images }) => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
-   return (
+  return (
     <div className="carrousel">
-      <button className="left-arrow" onClick={prevSlide}>❮</button>
+      {images.length > 1 && (
+        <button className="left-arrow" onClick={prevSlide}>❮</button>
+      )}
       <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="carrousel-image" />
-      <button className="right-arrow" onClick={nextSlide}>❯</button>
+      {images.length > 1 && (
+        <button className="right-arrow" onClick={nextSlide}>❯</button>
+      )}
       <div className="image-counter-container">
         <div className="image-counter">
           {currentIndex + 1}/{images.length}
